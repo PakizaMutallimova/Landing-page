561dbb1415e1015cb91c69d5efddc82a1fe2d171
PakizaMutallimova <pakizemutellimova97@mail.ru>
2024-10-21 23:24:55 +0400

    changes

--- demo.themesoul.com/nyabiss/index.php/home/main.js
+++ demo.themesoul.com/nyabiss/index.php/home/main.js
@@ -15,12 +15,12 @@ const socialsData = {
 
 
 
-const data = {
-    "fullname": "string",
-    "companyName": "string",
-    "email": "string",
-    "phone": "string"
-}
+// const data = {
+//     "fullname": "string",
+//     "companyName": "string",
+//     "email": "string",
+//     "phone": "string"
+// }
 
 const name = document.getElementById('name');
 const email = document.getElementById('email');
@@ -28,8 +28,23 @@ const number = document.getElementById('number');
 const company = document.getElementById('company');
 const btnSubmit = document.querySelector('.btn-submit')
 
-btnSubmit.addEventListener('click', () => {
-    console.log(name.value, email.value, number.value, company.value);
+btnSubmit.addEventListener('click', async () => {
+    const data = {
+        "fullname": name.value,
+        "companyName": company.value,
+        "email": email.value,
+        "phone": number.value
+    }
+    // console.log(data);
+    fetch('/notification/api/v2/Email/Partnership', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        // headers: {
+        //     'Content-type': 'application/json; charset=UTF-8',
+        // },
+    })
+        .then(async (response) => console.log(await response.json()))
+        .then((json) => console.log(json));
 })
 
 
